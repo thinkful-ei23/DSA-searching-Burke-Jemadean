cf4a0683bf5d1aa6a1f87dc879549a0c56572500
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-10 11:28:26 -0700

    minor changes

--- binary-vs-linear/src/App.js
+++ binary-vs-linear/src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
     this.state = {
       data: [1, 2, 3, 5, 6, 6, 6, 7, 7, 9, 9, 11, 13, 13, 13, 14, 14, 15, 16, 16, 17, 21, 22, 23, 24, 25, 25, 26, 26, 27, 27, 27, 28, 28, 28, 30, 31, 32, 32, 33, 34, 38, 38, 39, 40, 40, 42,
         42, 43, 44, 45, 46, 46, 46, 48, 49, 50, 51, 51, 53, 53, 54, 55, 56, 62, 63, 64, 64, 64, 65, 67, 68, 69, 69, 70, 70, 72, 72, 73, 73, 76, 78, 78, 80, 81, 82, 83, 84, 85, 87, 87, 88, 88, 89, 90, 91, 93, 97, 98, 98],
-      count: 0, error: null}
+      count: 0, 
+      error: null}
   }
 
   binarySearch(array, value, start, end, count=0) {
@@ -57,20 +58,19 @@ clearValues() {
 
 callIndexOf(array, value) {
   this.clearValues();
-  const found = this.indexOf(array, value);
-  if (found === -1) {
+  const index = this.indexOf(array, value);
+  if (index === -1) {
     this.setState({error: 'Your value is not in the array'});
   }
-  console.log(this.state);
   this.textInput.value = '';
 }
 
 callBinarySearch(array, value) {
   this.clearValues();
   console.log('in binary search');
-  const found = this.binarySearch(array, value);
-  if (found === -1) {
-    this.setState({error: 'Value not in array'});
+  const index = this.binarySearch(array, value);
+  if (index === -1) {
+    this.setState({error: 'Your value is not in the array'});
   }
   this.textInput.value = '';
 }
