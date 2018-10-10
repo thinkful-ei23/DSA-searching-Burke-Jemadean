51b49bad3d734f4d99113690b82dd3cfa488dab8
Burkey Koontz <bk.koontz@gmail.com>
2018-10-10 08:47:21 -0700

    fixed uncontrolled ref

--- binary-vs-linear/src/App.js
+++ binary-vs-linear/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.textInput = React.createRef();
+    // this.textInput = React.createRef();
     this.state = {data: [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]}
   }
 
@@ -47,23 +47,22 @@ indexOf(array, value) {
 callIndexOf(array, value) {
   console.log('in call index of');
   this.indexOf(array, value);
-  this.textInput = '';
+  this.textInput.value = '';
 }
 
 callBinarySearch(array, value) {
   console.log('in binary search');
   this.binarySearch(array, value);
-  this.textInput = '';
+  this.textInput.value = '';
 }
-
 
   render() {
     return (
       <div className="App">
         <form onSubmit={(e) => e.preventDefault()}>
-          <input ref={this.textInput} type="text"></input>
-          <button onClick={() => this.callIndexOf(this.state.data, this.textInput)}>Linear Search</button>
-          <button onClick={() => this.callBinarySearch(this.state.data, this.textInput)}>Binary Search</button>
+          <input ref={input => this.textInput = input} type="text"></input>
+          <button onClick={() => this.callIndexOf(this.state.data, this.textInput.value)}>Linear Search</button>
+          <button onClick={() => this.callBinarySearch(this.state.data, this.textInput.value)}>Binary Search</button>
         </form>
       </div>
     );
